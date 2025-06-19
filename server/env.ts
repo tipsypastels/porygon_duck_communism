1c4dd7a16154e75b334db23f6c3cbb7c956bc245
tipsypastels <dakotasankey@gmail.com>
2025-06-18 22:37:24 -0700

    required env vars

--- server/env.ts
+++ server/env.ts
@@ -1,1 +1,12 @@
-export const PUBLIC_KEY = Deno.env.get("DISCORD_PUBLIC_KEY");
+import { assert } from "@std/assert";
+
+export const DEV = Deno.env.get("DEV") == "1";
+
+export const GUILD_ID = required("DISCORD_GUILD_ID");
+export const PUBLIC_KEY = required("DISCORD_PUBLIC_KEY");
+
+function required(name: string) {
+  const value = Deno.env.get(name);
+  assert(value, `Missing env var '${name}'`);
+  return value;
+}
--- server/mod.ts
+++ server/mod.ts
@@ -2,4 +2,4 @@ import { Hono } from "hono";
 import nacl from "tweetnacl";
 import { Buffer } from "node:buffer";
 
-export const hono = new Hono();
+export const hono = new Hono().post("/");
