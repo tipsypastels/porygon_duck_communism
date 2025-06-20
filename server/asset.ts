fe9893dba581543447f2a4c01252ec686f0ff337
tipsypastels <dakotasankey@gmail.com>
2025-06-19 22:04:04 -0700

    don't use join

--- server/asset.ts
+++ server/asset.ts
@@ -1,7 +1,5 @@
-import { join } from "@std/path";
-
 const BASE = "https://porygon.deno.dev/assets";
 
 export function asset(path: string) {
-  return join(BASE, path);
+  return `${BASE}/${path}`;
 }
