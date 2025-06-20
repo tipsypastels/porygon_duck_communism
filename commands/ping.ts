0f8cddfbb154a6cf553c77174203e4d634955f95
tipsypastels <dakotasankey@gmail.com>
2025-06-19 21:40:07 -0700

    try on github instead

--- commands/ping.ts
+++ commands/ping.ts
@@ -3,7 +3,9 @@ import { Command } from "../server/command/mod.ts";
 const ping: Command = ({ embed, setEphemeral }) => {
   setEphemeral();
   embed
-    .face("speech.png")
+    .thumbnail(
+      "https://github.com/tipsypastels/porygon_duck_communism/blob/main/public/assets/faces/speech.png?raw=true",
+    )
     .color("info")
     .title(":sparkles: Pong! Porygon is online~")
     .description("_beep boop_ How are you today?");
