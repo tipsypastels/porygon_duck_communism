2bee4477d11f45809316675f0af4cb4119ddf5bc
tipsypastels <dakotasankey@gmail.com>
2025-06-19 21:50:23 -0700

    COME ON

--- commands/ping.ts
+++ commands/ping.ts
@@ -3,7 +3,9 @@ import { Command } from "../server/command/mod.ts";
 const ping: Command = ({ embed, setEphemeral }) => {
   setEphemeral();
   embed
-    .face("speech.png")
+    .thumbnail(
+      "https:/raw.githubusercontent.com/tipsypastels/porygon_duck_communism/refs/heads/main/assets/faces/speech.png",
+    )
     .color("info")
     .title(":sparkles: Pong! Porygon is online~")
     .description("_beep boop_ How are you today?");
