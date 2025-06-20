64c6361ea87fb9fff5a6579802e8612df9d1bcf1
tipsypastels <dakotasankey@gmail.com>
2025-06-19 21:37:27 -0700

    pray

--- commands/ping.ts
+++ commands/ping.ts
@@ -3,10 +3,7 @@ import { Command } from "../server/command/mod.ts";
 const ping: Command = ({ embed, setEphemeral }) => {
   setEphemeral();
   embed
-    // .face("speech.png")
-    .thumbnail(
-      "https://github.com/tipsypastels/porygon2/blob/master/assets/porygon2_avatar.png?raw=true",
-    )
+    .face("speech.png")
     .color("info")
     .title(":sparkles: Pong! Porygon is online~")
     .description("_beep boop_ How are you today?");
