f91feca86935e398f2b704c182baa7fc14e30c78
tipsypastels <dakotasankey@gmail.com>
2025-06-19 21:58:19 -0700

    bl

--- commands/ping.ts
+++ commands/ping.ts
@@ -3,7 +3,7 @@ import { Command } from "../server/command/mod.ts";
 const ping: Command = ({ embed, setEphemeral }) => {
   setEphemeral();
   embed
-    .face("speech.png")
+    .thumbnail("http://porygon.deno.dev/porygon.png")
     .color("info")
     .title(":sparkles: Pong! Porygon is online~")
     .description("_beep boop_ How are you today?");
