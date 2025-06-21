49ae761b56ef7ab170c20698c4c63775c2d45b73
tipsypastels <dakotasankey@gmail.com>
2025-06-20 17:34:19 -0700

    listenQueue is blocking

--- scripts/serve.ts
+++ scripts/serve.ts
@@ -1,9 +1,11 @@
 import { addAllCommands } from "../commands/mod.ts";
 import { registrar } from "../server/command/registrar.ts";
 import { DEV } from "../server/env.ts";
+import { startQueues } from "../server/kv.ts";
 import { hono } from "../server/mod.ts";
 
 addAllCommands();
+startQueues();
 
 if (DEV) {
   await registrar.register({ writeManifest: false });
--- server/kv.ts
+++ server/kv.ts
@@ -1,34 +1,38 @@
 export const kv = await Deno.openKv();
 
-export const createQueue = (() => {
+export const { startQueues, createQueue } = (() => {
   type Handler<T> = (event: T) => void | Promise<void>;
   type Message<T> = { key: symbol; event: T };
 
   const handlers = new Map<symbol, Handler<never>>();
 
-  kv.listenQueue(async (message: Message<never>) => {
-    if (
-      !message ||
-      typeof message !== "object" ||
-      typeof message.key !== "symbol"
-    ) {
-      return void console.warn(`KV queue got invalid message: ${message}`);
-    }
+  function startQueues() {
+    kv.listenQueue(async (message: Message<never>) => {
+      if (
+        !message ||
+        typeof message !== "object" ||
+        typeof message.key !== "symbol"
+      ) {
+        return void console.warn(`KV queue got invalid message: ${message}`);
+      }
 
-    const handler = handlers.get(message.key);
-    if (!handler) {
-      return void console.warn(`KV queue got unhandled message type`);
-    }
+      const handler = handlers.get(message.key);
+      if (!handler) {
+        return void console.warn(`KV queue got unhandled message type`);
+      }
 
-    await handler(message.event);
-  });
+      await handler(message.event);
+    });
+  }
 
-  return <T>(handler: Handler<T>) => {
+  function createQueue<T>(handler: Handler<T>) {
     const key = Symbol();
     handlers.set(key, handler);
 
     return async (event: T) => {
       await kv.enqueue({ key, event } satisfies Message<T>);
     };
-  };
+  }
+
+  return { startQueues, createQueue };
 })();
