73834961c96e2ddb8f2b317b869eafaa585a9790
tipsypastels <dakotasankey@gmail.com>
2025-06-20 17:36:21 -0700

    use uuids for handlers

--- server/kv.ts
+++ server/kv.ts
@@ -2,16 +2,16 @@ export const kv = await Deno.openKv();
 
 export const { startQueues, createQueue } = (() => {
   type Handler<T> = (event: T) => void | Promise<void>;
-  type Message<T> = { key: symbol; event: T };
+  type Message<T> = { key: string; event: T };
 
-  const handlers = new Map<symbol, Handler<never>>();
+  const handlers = new Map<string, Handler<never>>();
 
   function startQueues() {
     kv.listenQueue(async (message: Message<never>) => {
       if (
         !message ||
         typeof message !== "object" ||
-        typeof message.key !== "symbol"
+        typeof message.key !== "string"
       ) {
         return void console.warn(`KV queue got invalid message: ${message}`);
       }
@@ -26,7 +26,7 @@ export const { startQueues, createQueue } = (() => {
   }
 
   function createQueue<T>(handler: Handler<T>) {
-    const key = Symbol();
+    const key = crypto.randomUUID();
     handlers.set(key, handler);
 
     return async (event: T) => {
