091b23a4b6c093875d9f8ffc010fd1640c428e68
tipsypastels <dakotasankey@gmail.com>
2025-06-24 13:17:10 -0700

    don't limit the members

--- server/discord/rest.ts
+++ server/discord/rest.ts
@@ -5,8 +5,10 @@ import { BOT_ID, BOT_TOKEN, GUILD_ID } from "./env.ts";
 
 const rest = new REST().setToken(BOT_TOKEN);
 
-export async function getGuildMembers() {
-  return await rest.get(R.guildMembers(GUILD_ID)) as D.APIGuildMember[];
+export async function getGuildMembers({ limit = 1000 }) {
+  return await rest.get(R.guildMembers(GUILD_ID), {
+    query: new URLSearchParams({ limit: limit.toString() }),
+  }) as D.APIGuildMember[];
 }
 
 export async function postApplicationGuildCommand(
